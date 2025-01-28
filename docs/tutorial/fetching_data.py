aac8344240f00b11233b4a4dc887ee5699b91312
David Stansby <dstansby@gmail.com>
2025-01-28 16:50:26 +0000

    Convert fetching data tutorial

---
+++ docs/tutorial/fetching_data.py
@@ -0,0 +1,39 @@
+# # Fetching data
+
+# In this tutorial we will fetch some of the data. All public Human Organ Atlas
+# data is available through a read-only public Google Cloud Storage bucket in
+# the N5 data format. This is a chunked data format, making it easy to access
+# sub-volumes of data.
+
+# ## Getting a remote data store
+
+# First we'll fetch a `Dataset` object from the inventory.
+
+import hoa_tools.inventory
+import hoa_tools.dataset
+
+inventory = hoa_tools.inventory.load_inventory()
+dataset_name = inventory.index[0]
+print(dataset_name)
+
+dataset = hoa_tools.dataset.get_dataset(dataset_name)
+print(dataset)
+
+# The `Dataset.remote_array` property gives us access to a remote data array for a dataset.
+# The ``level`` parameter lets us choose a downsampling level: ``level=0`` is the original
+# resolution dataset, and each time the level goes up by one the dataset is downsampled
+# by a factor of 2. All datasets are downsampled to ``level=4``, so lets get a remote store
+# for the lowest resolution copy of the data.
+
+remote_array_l4 = dataset.remote_array(level=4)
+print(remote_array_l4)
+
+# Here we can see that the array is read-only, 16 bit, and has shape ``(477, 238, 238)``.
+# At this point no data has been downloaded - to download data you need to index the remote array.
+# As an example, lets fetch and show a slice in the x-y plane::
+
+import matplotlib.pyplot as plt
+import skimage.exposure
+
+middle_slice = remote_array_l4[437, :, :]
+plt.imshow(skimage.exposure.equalize_hist(middle_slice))
