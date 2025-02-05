2ef7abe9e72a84892fa35d74829508bc79d617a0
David Stansby <dstansby@gmail.com>
2025-02-05 13:56:57 +0000

    Improve VOI example

--- docs/tutorial/vois.py
+++ docs/tutorial/vois.py
@@ -3,26 +3,27 @@
 # In this tutorial we will look at how to represent volumes of
 # interest in datasets.
 
+# +
 import matplotlib.pyplot as plt
-import skimage.exposure
 
 import hoa_tools.dataset
-from hoa_tools.voi import VOI
+import hoa_tools.voi
 
-# First we'll get a dataset, and inspect the data array
+# -
+
+# First we'll get a dataset.
 
 dataset = hoa_tools.dataset.get_dataset(
     "LADAF-2020-27_spleen_complete-organ_25.08um_bm05"
 )
-dataset.data_array(downsample_level=4)
 
-# Now we'll define a volume of interest (VOI) object
+# Now we'll define a volume of interest (VOI) object. This represents a cuboid shape area within our dataset.
 
-voi = VOI(
+voi = hoa_tools.voi.VOI(
     dataset=dataset,
     downsample_level=4,
     lower_corner={"x": 100, "y": 50, "z": 23},
-    size={"x": 20, "y": 10, "z": 1},
+    size={"x": 40, "y": 20, "z": 1},
 )
 
 # Once we have a VOI, there's a number of useful things we can do.
@@ -31,27 +32,32 @@ voi = VOI(
 data_array = voi.get_data_array()
 data_array
 
-data_array.values = skimage.exposure.equalize_hist(data_array.values)
 plt.figure()
 ax = data_array.plot(cmap="Grays_r")
 ax.axes.set_aspect("equal")
+ax.axes.set_title("Downsample level = 4")
 
 
-# ## Transforming VOIs
+# ## Transforming between dowsample levels
 #
 # VOIs can be transformed to the different resolution levels of the same dataset.
 # Lets get a higher resolution version of the data we plotted above:
 
-new_voi = voi.change_downsample_level(new_downsample_level=2)
-new_voi
+high_res_voi = voi.change_downsample_level(new_downsample_level=2)
 
 # Because we've increased the resolution (equivalently, decreased the downsampling level)
 # the VOI is now bigger.
 
-new_data_array = new_voi.get_data_array()
-new_data_array
+high_res_data_array = high_res_voi.get_data_array()
 
-new_data_array.values = skimage.exposure.equalize_hist(new_data_array.values)
-plt.figure()
-ax = new_data_array.isel(z=0).plot(cmap="Grays_r")
-ax.axes.set_aspect("equal")
+# +
+fig, axs = plt.subplots(nrows=2)
+high_res_data_array.isel(z=0).plot(cmap="Grays_r", ax=axs[0])
+axs[0].set_aspect("equal")
+axs[0].set_title("Downsample level = 2")
+
+data_array.isel(z=0).plot(cmap="Grays_r", ax=axs[1])
+axs[1].set_aspect("equal")
+axs[1].set_title("Downsample level = 4")
+
+fig.tight_layout()
